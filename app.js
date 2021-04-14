dec062c46f33f8cc052b955df9c7bc8e2662c3d2
AngelDOrtiz <aortizghs@gmail.com>
2021-04-14 16:04:59 -0700

    added some localstorage functions

--- app.js
+++ app.js
@@ -3,3 +3,13 @@
 // initialize state
 
 // set event listeners to update state and DOM
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    
+    const username = formData.get('username');
+    const password = formData.get('password');
+    
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,24 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { createUser } from '../local-storage-utils.js';
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
+    const username = 'angel';
+    const password = '1234';
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = {
+        username: 'angel',
+        password: '1234',
+        todos: []
+    };
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = createUser(username, password);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.deepEqual(actual, expected);
 });
