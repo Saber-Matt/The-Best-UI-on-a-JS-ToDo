5233f7860bbaed7ecaf54e97732b8a130ef92937
David Delgadillo <daviddel.731@gmail.com>
2021-04-15 10:09:06 -0700

    Worked on render functions

--- render-utils.js
+++ render-utils.js
@@ -1,1 +1,25 @@
-import getUser from './local-storage-utils.js';
+import { getUser } from './local-storage-utils.js';
+
+export function renderLi(todo) {
+    const li = document.createElement('li');
+    li.textContent = todo.todo;
+   
+    return li;
+}
+
+export function renderTodos() {
+    const user = getUser();
+
+    const ul = document.getElementById('toDoList');
+
+    user.todos.forEach(todo => {
+        const li = renderLi(todo);
+
+        ul.append(li);
+    });
+
+}
+
+
+
+
